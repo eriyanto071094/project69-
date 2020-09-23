fdf3b1000e833aa0fc68d8fe1272a0c5c170f3bb
eriyanto071094 <71745455+eriyanto071094@users.noreply.github.com>
2020-09-23 11:15:12 +0700

    Add files via upload

---
+++ database/migrations/2020_09_23_020754_create_siswa_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('nis');
+            $table->string('nama_siswa');
+            $table->string('alamat');
+            $table->integer('no_hp')->length(20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
